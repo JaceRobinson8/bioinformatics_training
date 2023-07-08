0ba490265f4cee7e0830bc1742031b398bcdcf49
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2023-07-08 13:52:12 -0400

    query neo4j db

---
+++ notes/notes.md
@@ -0,0 +1,15 @@
+# Start Day 7-8-23
+
+1. Learning neo4j for bioinformatics
+
+### Notes on neo4j for bio
+
+Going through the neo4j setup on auradb.
+
+I should learn self hosted with docker, but for now using the free tier in the cloud is fine.
+
+Goal to get neo4j setup. Learn the basics. Then shove any biology based dataset in there. Then decide some data science tasks after. But for now just get some data in there!
+
+neo4j is super cool!
+
+How do they handle properties?
--- src/main.py
+++ src/main.py
@@ -1,3 +1,37 @@
-import pandas as pd
+from neo4j import GraphDatabase
+import json
+from pathlib import Path
+import logging
+from rich.logging import RichHandler
 
-print("hello world")
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level=logging.INFO, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+file_path = Path("secrets/Neo4j-6dcf787b-Created-2023-07-08.json")
+
+with open(file_path, 'r') as myfile:
+    neo4j_creds = json.load(myfile)
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = neo4j_creds["NEO4J_URI"]
+AUTH = (neo4j_creds["NEO4J_USERNAME"], neo4j_creds["NEO4J_PASSWORD"])
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+
+    # Get the name of all 42 year-olds
+    records, summary, keys = driver.execute_query(
+        "MATCH (n:Category) RETURN n LIMIT 25;"
+    )
+
+    # Loop through results and do something with them
+    for cat in records:
+        logging.info(cat)
+
+    # Summary information
+    logging.info("The query `{query}` returned {records_count} records in {time} ms.".format(
+        query=summary.query, records_count=len(records),
+        time=summary.result_available_after,
+    ))
